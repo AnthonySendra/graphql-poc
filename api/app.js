fc0b7de6932f7e60c4200cffb071bdcb4dfbdd65
Anthony Sendra <asendra@kaliop.com>
2017-02-06 14:24:53 +0100

    update gitignore

--- api/app.js
+++ api/app.js
@@ -1,15 +1,14 @@
 'use strict'
 
 let express = require('express')
-let OpticsAgent = require('optics-agent');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 var cors = require('cors')
-
-OpticsAgent.configureAgent({ apiKey: 'service:AnthonySendra-test:XPM_lU-3de-FIIuyHWhHug' })
+var fs = require('fs')
+var path = require('path')
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(require('./schemas/hello.graphqls'));
+var schema = buildSchema(String(fs.readFileSync(path.join(__dirname, 'lib', 'graphql', 'schema.graphqls'))))
 
 // The root provides a resolver function for each API endpoint
 var root = {
@@ -21,15 +20,12 @@ var root = {
 var app = express();
 
 app.use(cors())
-OpticsAgent.instrumentSchema(schema);
-app.use('/graphql', OpticsAgent.middleware());
 app.use('/graphql', graphqlHTTP(req => {
   return {
     schema: schema,
     rootValue: root,
-    graphiql: true,
-    context: {opticsContext: OpticsAgent.context(req)}
+    graphiql: true
   }
-}));
-app.listen(4000);
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+}))
+app.listen(4000)
+console.log('Running a GraphQL API server at localhost:4000/graphql')
